531f30d89a19cb02405919cdeccf75cccabdf653
vira2121 <virajadhav2121@gmail.com>
2022-03-28 17:13:32 +0530

    done successfully

--- test_main.py
+++ test_main.py
@@ -1,48 +1,48 @@
-import main
+import divisibility
 
 def test_divby5():
     a = 10
-    out1 = main.divby5(a)
+    out1 = divisibility.divby5(a)
     assert out1 == True
 
 
 def test_divby5_1():
     a = 14
-    out1 = main.divby5(a)
+    out1 = divisibility.divby5(a)
     assert out1 == False
 
 
 def test_divby7():
     a = 10
-    out1 = main.divby7(a)
+    out1 = divisibility.divby7(a)
     assert out1 == False
 
 
 def test_divby7_1():
     a = 14
-    out1 = main.divby7(a)
+    out1 = divisibility.divby7(a)
     assert out1 == True
 
 
 def test_divby9():
     a = 10
-    out1 = main.divby9(a)
+    out1 = divisibility.divby9(a)
     assert out1 == False
 
 
 def test_divby9_1():
     a = 18
-    out1 = main.divby9(a)
+    out1 = divisibility.divby9(a)
     assert out1 == True
 
 
 def test_divby11():
     a = 10
-    out1 = main.divby11(a)
+    out1 = divisibility.divby11(a)
     assert out1 == False
 
 
 def test_divby11_1():
     a = 121
-    out1 = main.divby11(a)
+    out1 = divisibility.divby11(a)
     assert out1 == True
